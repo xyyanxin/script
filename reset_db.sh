06ad26ac91b543a5df779fea8c9b02b7f4a61d6a
xy <yanxin@chengantech.com>
2017-08-02 13:15:43 +0800

    daily backup

--- reset_db.sh
+++ reset_db.sh
@@ -1,7 +1,7 @@
-cd '/home/xy/workspace/Even'
+cd '/home/xy/workspace/Bond'
 ps aux|grep "runserver"|awk '{print $2}'|xargs kill -9
 echo "y"|python manage.py dropdb
 echo "y"|python manage.py initdb
 echo "y"|python manage.py initdata
-python /home/xy/workspace/Even/manage.py runserver
+python /home/xy/workspace/Bond/manage.py runserver
 echo 'ok! data in reset !!!!'
